49f6ba1448337fc2d7156ab84b030ad0b7b7f774
Nicholas Todoroff <loppy1243@gmail.com>
2018-10-20 22:48:29 -0400

    Change implementation of plotpoint!

--- src/Data.jl
+++ src/Data.jl
@@ -4,15 +4,29 @@ export COLUMNS, GRIDSIZE, CELLS, MAX_E, XYMIN, XYMAX, XYOFF
 using Base.Iterators: drop, take
 using IterTools: takenth, chain
 using Lazy: @>
+using RecipesBase
 
 const COLUMNS = 16*16 + 6
-const GRIDSIZE = [16, 16]
+#const GRIDSIZE = [16, 16]
 const CELLS = prod(GRIDSIZE)
 const MAX_E = 3060.0
 
-const XYMIN = -[48.0, 48.0]/2 # = [-24, -24]
-const XYMAX = [48.0, 48.0]/2 # = [24, 24]
-const XYOFF = -[48.0, 48.0]/2 # = [-24, -24]
+#const XYMIN = -[48.0, 48.0]/2 # = [-24, -24]
+#const XYMAX = [48.0, 48.0]/2 # = [24, 24]
+#const XYOFF = -[48.0, 48.0]/2 # = [-24, -24]
+
+struct Grid
+    size::Tuple{Int, Int}
+    xymin::Tuple{Float64, Float64}
+    xymax::Tuple{Float64, Float64}
+    xyoff::Tuple{Float64, Float64}
+end
+Base.getproperty(grid::Grid, x::Symbol) = if x === :numcells
+    prod(grid.size)
+else
+    Base.getfield(grid, x)
+end
+const GRID = Grid((16, 16), (-48.0/2, -48.0/2), (48.0/2, 48.0/2), (-48.0/2, -48.0/2))
 
 read(file; kws...) = read(Float64, file; kws...)
 read(file, range; kws...) = read(Float64, file, range; kws...)
@@ -66,8 +80,26 @@ function plotpoint!(plt, p; kws...)
 
     xy = @. (p - XYOFF)/(XYMAX-XYMIN)*[xmax-xmin, ymax-ymin] + [xmin, ymin]
 
-    scatter!(plt, [xy[1]], [xy[2]], legend=false; kws...)
+    plot!(plt, [xy[1]], [xy[2]]; legend=false, seriestype=:scatter, kws...)
 end
 plotpoint!(p; kws...) = plotpoint!(Plots.current(), p; kws...)
+
+#@recipe function plotgrid(grid::Grid, data::Array{Float64, 2})
+#    seriestype := heatmap
+#    legend := false
+#
+#    (axes(data, 2), reverse(axes(data, 1)), data)
+#end
+#@recipe function gridscatter(grid::Grid, point::Vector{Int})
+#    seriestype := scatter
+#    legend := false
+#
+#    xmin, xmax = plotattributes[:xmin], plotattributes[:xmax]
+#    ymin, ymax = plotattributes[:ymin], plotattributes[:ymax]
+#
+#    xy = @. (p - grid.xyoff)/(grid.xymax-grid.xymin)*[xmax-xmin, ymax-ymin] + [xmin, ymin]
+#
+#    ([xy[1]], [xy[2]])
+#end
 
 end # module Data
